d6f2c3f952ca2f3369a664beda1439eeac669538
anshdhariwal <anshdhariwalhsr@gmail.com>
2026-05-03 21:16:33 +0530

    fix: add STATIC_ROOT and update ALLOWED_HOSTS for production

--- voting_system/settings.py
+++ voting_system/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-change-in-production')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'anshdhariwal.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -81,6 +81,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
